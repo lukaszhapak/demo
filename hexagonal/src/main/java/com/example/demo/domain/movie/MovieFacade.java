e7ad55c9839f773689c8758c98012541f1a53ca9
lukasz.hapak <lukasz.hapak@britenet.com.pl>
2022-10-28 16:12:13 +0200

    logging

--- hexagonal/src/main/java/com/example/demo/domain/movie/MovieFacade.java
+++ hexagonal/src/main/java/com/example/demo/domain/movie/MovieFacade.java
@@ -1,7 +1,9 @@
 package com.example.demo.domain.movie;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 
+@Log4j2
 @RequiredArgsConstructor
 public class MovieFacade {
 
@@ -9,11 +11,14 @@ public class MovieFacade {
   private final MovieCreatedEventSender movieCreatedEventSender;
 
   public Movie createMovie(Movie movie) {
+	log.debug("create movie Movie={}", movie);
+	Movie response = movieStorage.create(movie);
 	movieCreatedEventSender.send(MovieCreatedEvent.of(movie));
-	return movieStorage.create(movie);
+	return response;
   }
 
   public Movie findById(Long id) {
+	log.debug("find movie by id={}", id);
 	return movieStorage.findById(id)
 		.orElseThrow(() -> new MovieNotFoundException(id));
   }
