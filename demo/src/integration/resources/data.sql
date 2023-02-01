89242003111009ed0c9f7fba2c0b2a13cd3bfaac
lukasz.hapak <lukasz.hapak@britenet.com.pl>
2023-02-01 20:26:13 +0100

    comment in data script

--- demo/src/integration/resources/data.sql
+++ demo/src/integration/resources/data.sql
@@ -1,4 +1,4 @@
 INSERT INTO STUDENT(ID, NAME, AGE, GRADES)
-VALUES (1000001, 'John', 22, '2,5,4,3,3'),
-       (1000002, 'John', 22, '2,5,4,3,3'),
-       (1000003, 'John', 22, '2,5,4,3,3');
+VALUES (1000001, 'John', 22, '2,5,4,3,3'), -- get student
+       (1000002, 'John', 22, '2,5,4,3,3'), -- delete student
+       (1000003, 'John', 22, '2,5,4,3,3'); -- update student
