7c275879b5165574b681a650b313720a696fbd4f
lukasz.hapak <lukasz.hapak@britenet.com.pl>
2023-02-05 17:23:26 +0100

    generated module

---
+++ generated/src/main/java/com/example/generated/GeneratedApplication.java
@@ -0,0 +1,12 @@
+package com.example.generated;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneratedApplication {
+
+  public static void main(String[] args) {
+	SpringApplication.run(GeneratedApplication.class, args);
+  }
+}
