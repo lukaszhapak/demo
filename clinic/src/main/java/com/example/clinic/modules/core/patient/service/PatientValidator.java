85a3843b4deff59da067dd34b40582d78950a093
lukasz.hapak <lukasz.hapak@britenet.com.pl>
2023-04-30 22:08:56 +0200

    validation in clinic

--- clinic/src/main/java/com/example/clinic/modules/core/patient/service/PatientValidator.java
+++ clinic/src/main/java/com/example/clinic/modules/core/patient/service/PatientValidator.java
@@ -8,26 +8,10 @@ public class PatientValidator extends AbstractValidator {
 
   public void validate(PatientDTO patientDTO) {
 	invalidFields = new HashMap<>();
-	validateFirstName(patientDTO.getFirstName());
-	validateLastName(patientDTO.getLastName());
-	validatePesel(patientDTO.getPesel());
-	validatePhoneNumber(patientDTO.getPhoneNumber());
+	validateString(patientDTO.getFirstName(), 2, 64, "firstName");
+	validateString(patientDTO.getLastName(), 2, 64, "lastName");
+	validateString(patientDTO.getPesel(), 11, 11, "pesel");
+	validateString(patientDTO.getPhoneNumber(), 9, 9, "phoneNumber");
 	throwException("Patient");
-  }
-
-  private void validateFirstName(String firstName) {
-	validateString(firstName, 2, 64, "firstName");
-  }
-
-  private void validateLastName(String lastName) {
-	validateString(lastName, 2, 64, "lastName");
-  }
-
-  private void validatePesel(String pesel) {
-	validateString(pesel, 11, 11, "pesel");
-  }
-
-  private void validatePhoneNumber(String phoneNumber) {
-	validateString(phoneNumber, 9, 9, "phoneNumber");
   }
 }
