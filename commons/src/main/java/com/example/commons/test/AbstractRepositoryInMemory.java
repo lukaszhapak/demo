b18fa4519c82446c40a43b9e0de9c17e38fd3f99
lukasz.hapak <lukasz.hapak@britenet.com.pl>
2023-08-26 13:04:48 +0200

    commons refactor

--- commons/src/main/java/com/example/commons/test/AbstractRepositoryInMemory.java
+++ commons/src/main/java/com/example/commons/test/AbstractRepositoryInMemory.java
@@ -1,51 +1,31 @@
 package com.example.commons.test;
 
 import com.example.commons.commons.AbstractEntity;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 import org.springframework.data.repository.Repository;
 
 public abstract class AbstractRepositoryInMemory<T extends AbstractEntity> implements Repository<T, Long> {
 
-  protected List<T> list = new ArrayList<>();
+  protected Map<Long, T> map = new HashMap<>();
   private Long id = 0L;
 
   protected T save(T entity) {
-	if (entity.getId() == null || !existsById(entity.getId())) {
+	if (entity.getId() == null || !map.containsKey(entity.getId())) {
 	  setId(entity);
 	}
-	if (existsById(entity.getId())) {
-	  deleteAllById(entity.getId());
-	}
-	list.add(entity);
+	map.put(entity.getId(), entity);
 	return entity;
   }
 
   protected Optional<T> findById(Long id) {
-	return list.stream()
-		.filter(x -> Objects.equals(x.getId(), id))
-		.findFirst();
+	return Optional.ofNullable(map.get(id));
   }
 
   protected Long deleteAllById(Long id) {
-	Optional<T> entity = list.stream()
-		.filter(x -> Objects.equals(x.getId(), id))
-		.findFirst();
-	if (entity.isPresent()) {
-	  list.remove(entity.get());
-	  return 1L;
-	} else {
-	  return 0L;
-	}
-  }
-
-  protected boolean existsById(Long id) {
-	Optional<T> entity = list.stream()
-		.filter(x -> Objects.equals(x.getId(), id))
-		.findFirst();
-	return entity.isPresent();
+	T removed = map.remove(id);
+	return removed == null ? 0L : 1L;
   }
 
   private void setId(T entity) {
---
+++ commons/src/main/java/com/example/commons/test/AbstractRepositoryInMemoryList.java
@@ -0,0 +1,54 @@
+package com.example.commons.test;
+
+import com.example.commons.commons.AbstractEntity;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import org.springframework.data.repository.Repository;
+
+public abstract class AbstractRepositoryInMemoryList<T extends AbstractEntity> implements Repository<T, Long> {
+
+  protected List<T> list = new ArrayList<>();
+  private Long id = 0L;
+
+  protected T save(T entity) {
+	if (entity.getId() == null || !existsById(entity.getId())) {
+	  setId(entity);
+	}
+	if (existsById(entity.getId())) {
+	  deleteAllById(entity.getId());
+	}
+	list.add(entity);
+	return entity;
+  }
+
+  protected Optional<T> findById(Long id) {
+	return list.stream()
+		.filter(x -> Objects.equals(x.getId(), id))
+		.findFirst();
+  }
+
+  protected Long deleteAllById(Long id) {
+	Optional<T> entity = list.stream()
+		.filter(x -> Objects.equals(x.getId(), id))
+		.findFirst();
+	if (entity.isPresent()) {
+	  list.remove(entity.get());
+	  return 1L;
+	} else {
+	  return 0L;
+	}
+  }
+
+  protected boolean existsById(Long id) {
+	Optional<T> entity = list.stream()
+		.filter(x -> Objects.equals(x.getId(), id))
+		.findFirst();
+	return entity.isPresent();
+  }
+
+  private void setId(T entity) {
+	entity.setId(++id);
+  }
+}
