01d3bf30e8d82fe76b55f034234baaa917552bb7
lukasz.hapak <lukasz.hapak@britenet.com.pl>
2023-09-09 23:56:12 +0200

    refactor

---
+++ demo/src/main/java/com/example/demo/requestparam/Params.java
@@ -0,0 +1,16 @@
+package com.example.demo.requestparam;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Params {
+
+  private String name;
+  private String surname;
+  private int age;
+
+}
---
+++ demo/src/main/java/com/example/demo/requestparam/RequestParamController.java
@@ -0,0 +1,14 @@
+package com.example.demo.requestparam;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RequestParamController {
+
+  @GetMapping("/params")
+  public void get(Params params) {
+
+  }
+
+}
