e0c251896a795dba50de90e06353bdae043eb270
lukasz.hapak <lukasz.hapak@britenet.com.pl>
2023-09-12 13:44:37 +0200

    logs in clinic

--- clinic/src/main/java/com/example/clinic/modules/core/patient/service/PatientService.java
+++ clinic/src/main/java/com/example/clinic/modules/core/patient/service/PatientService.java
@@ -6,8 +6,10 @@ import com.example.clinic.modules.core.patient.repository.PatientRepository;
 import com.example.commons.exception.NotFoundException;
 import java.text.MessageFormat;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Transactional
 @RequiredArgsConstructor
 public class PatientService {
@@ -16,16 +18,19 @@ public class PatientService {
   private final PatientValidator patientValidator;
 
   public PatientDTO findById(Long id) {
+	log.debug("Getting patient id={}", id);
 	return getPatient(id).toDTO();
   }
 
   public PatientDTO save(PatientDTO patientDTO) {
+	log.debug("Saving patient patientDTO={}", patientDTO);
 	patientValidator.validate(patientDTO);
 	Patient patient = Patient.of(patientDTO);
 	return patientRepository.save(patient).toDTO();
   }
 
   public PatientDTO update(Long id, PatientDTO patientDTO) {
+	log.debug("Updating patient id={} patientDTO={}", id, patientDTO);
 	patientValidator.validate(patientDTO);
 	Patient toUpdate = getPatient(id);
 	toUpdate.update(patientDTO);
@@ -33,6 +38,7 @@ public class PatientService {
   }
 
   public void deleteById(Long id) {
+	log.debug("Deleting patient id={}", id);
 	if (patientRepository.deleteAllById(id) == 0) {
 	  throw new NotFoundException(MessageFormat.format("Patient with id={0} not found", id));
 	}
--- clinic/src/main/java/com/example/clinic/modules/core/patient/service/PatientValidator.java
+++ clinic/src/main/java/com/example/clinic/modules/core/patient/service/PatientValidator.java
@@ -3,10 +3,13 @@ package com.example.clinic.modules.core.patient.service;
 import com.example.clinic.modules.core.patient.model.PatientDTO;
 import com.example.commons.commons.AbstractValidator;
 import java.util.HashMap;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class PatientValidator extends AbstractValidator {
 
   public void validate(PatientDTO patientDTO) {
+	log.debug("Validating patient patientDTO={}", patientDTO);
 	invalidFields = new HashMap<>();
 	validateString(patientDTO.getFirstName(), 2, 64, "firstName");
 	validateString(patientDTO.getLastName(), 2, 64, "lastName");
