e8e2c7ec0e15c93b9e4a84c236959b70995534ff
lukasz.hapak <lukasz.hapak@britenet.com.pl>
2023-09-23 11:54:44 +0200

    refactor

--- demo/src/main/java/com/example/demo/query/ChildRepository.java
+++ demo/src/main/java/com/example/demo/query/ChildRepository.java
@@ -4,7 +4,7 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-public interface ChildRepository extends JpaRepository<Child, Long> {
+interface ChildRepository extends JpaRepository<Child, Long> {
 
   @Query("SELECT c from Child c LEFT JOIN FETCH c.parents p where c.id = :id")
   Optional<Child> findById(Long id);
--- demo/src/main/java/com/example/demo/query/ParentRepository.java
+++ demo/src/main/java/com/example/demo/query/ParentRepository.java
@@ -2,6 +2,6 @@ package com.example.demo.query;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ParentRepository extends JpaRepository<Parent, Long> {
+interface ParentRepository extends JpaRepository<Parent, Long> {
 
 }
