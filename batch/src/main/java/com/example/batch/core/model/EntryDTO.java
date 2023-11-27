dd909ec5e9ca3a4f0f4b7e5346a1054d686735ca
lukasz.hapak <lukasz.hapak@britenet.com.pl>
2023-11-27 17:53:36 +0100

    batch refactor

--- batch/src/main/java/com/example/batch/core/model/EntryDTO.java
+++ batch/src/main/java/com/example/batch/core/model/EntryDTO.java
@@ -2,9 +2,11 @@ package com.example.batch.core.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class EntryDTO {
 
   private Long id;
