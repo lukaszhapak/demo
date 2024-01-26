69f0a9b3f962323fcced63c8423e758cb459bfb6
lukasz.hapak <lukasz.hapak@britenet.com.pl>
2024-01-26 12:32:22 +0100

    properties

--- demo/src/main/java/com/example/demo/spring/properties/PropertiesService.java
+++ demo/src/main/java/com/example/demo/spring/properties/PropertiesService.java
@@ -1,9 +1,19 @@
 package com.example.demo.spring.properties;
 
+import java.util.List;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 class PropertiesService {
 
-  
+  @Value("#{${properties.map}}")
+  Map<String, String> mapFromProperties;
+
+  @Value("${properties.list}")
+  List<String> listFromProperties;
+
+  @Value("${properties.text}")
+  String textFromProperties;
 }
