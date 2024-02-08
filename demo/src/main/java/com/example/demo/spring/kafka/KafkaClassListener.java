3044185f75acd1d8e076860ffc8e206c9a1e2349
lukasz.hapak <lukasz.hapak@britenet.com.pl>
2024-02-08 17:54:05 +0100

    kafka consumer record

--- demo/src/main/java/com/example/demo/spring/kafka/KafkaClassListener.java
+++ demo/src/main/java/com/example/demo/spring/kafka/KafkaClassListener.java
@@ -1,6 +1,7 @@
 package com.example.demo.spring.kafka;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -9,7 +10,7 @@ import org.springframework.stereotype.Component;
 class KafkaClassListener {
 
   @KafkaListener(id = "demo-application", topics = "test-topic")
-  void listen(KafkaEvent kafkaEvent) {
+  void listen(ConsumerRecord<String, KafkaEvent> kafkaEvent) {
 	log.debug("Event received kafkaEvent={}", kafkaEvent);
   }
 }
