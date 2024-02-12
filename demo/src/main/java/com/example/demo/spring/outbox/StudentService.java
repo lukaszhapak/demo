a844fed0d9a089bf14093cb3b4b8c68cc7325d5f
lukasz.hapak <lukasz.hapak@britenet.com.pl>
2024-02-12 23:35:55 +0100

    transactional

--- demo/src/main/java/com/example/demo/spring/outbox/StudentService.java
+++ demo/src/main/java/com/example/demo/spring/outbox/StudentService.java
@@ -1,9 +1,9 @@
 package com.example.demo.spring.outbox;
 
-import javax.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
