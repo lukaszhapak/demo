9ad4b007c7dae0e4310b049cf999610d90ccbbba
lukasz.hapak <lukaszrdw@gmail.com>
2024-02-17 00:52:25 +0100

    flyway profile

---
+++ demo/src/main/resources/flyway/V1.0.1__create-student-table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE STUDENT (
+    ID              BIGSERIAL          PRIMARY KEY,
+    FIRST_NAME      VARCHAR(64)        NOT NULL,
+    LAST_NAME       VARCHAR(64)        NOT NULL,
+    GENDER          VARCHAR(6)         NOT NULL,
+    PESEL           VARCHAR(11)        UNIQUE NOT NULL,
+    PHONE_NUMBER    VARCHAR(9)         NOT NULL,
+    BIRTH_DATE      DATE               NOT NULL
+);
