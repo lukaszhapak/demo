08a0a5397c5839f157e26025b35fa2bed7b2be31
lukasz.hapak <lukaszrdw@gmail.com>
2024-02-26 22:19:11 +0100

    mongo deleted

--- demo/src/main/java/com/example/demo/spring/core/beanLifeCycle/Application.java
+++ demo/src/main/java/com/example/demo/spring/core/beanLifeCycle/Application.java
@@ -1,14 +1,19 @@
-package com.example.demo.spring.data.mongo;
+package com.example.demo.spring.core.beanLifeCycle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.context.annotation.Bean;
 
-@EnableMongoRepositories
 @SpringBootApplication
 class Application {
 
   public static void main(String[] args) {
 	SpringApplication.run(Application.class, args);
   }
+
+  @Bean(initMethod = "initMethod", destroyMethod = "destroyMethod")
+  Service service() {
+	return new Service();
+  }
+
 }
--- demo/src/main/java/com/example/demo/spring/data/mongo/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.spring.data.mongo;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-interface StudentRepository extends MongoRepository<Student, String> {
-
-}
