a54aefc39abdb5727551f9b2f8286f0877afafa1
lukasz.hapak <lukaszrdw@gmail.com>
2024-02-28 10:17:01 +0100

    mysql p6spy

---
+++ demo/src/test/java/com/example/demo/spring/data/databases/MysqlP6SpyTest.java
@@ -0,0 +1,36 @@
+package com.example.demo.spring.data.databases;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.example.demo.commons.AbstractIntegrationTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("mysql-p6spy") // requires mysql database, can be found in docker compose
+class MysqlP6SpyTest extends AbstractIntegrationTest {
+
+  @Autowired
+  StudentRepository studentRepository;
+
+  @Test
+  @DisplayName("save and get student")
+  void saveAndGetStudent() {
+	// given
+	Long id = studentRepository.save(createStudent()).getId();
+
+	// when
+	Student student = studentRepository.findById(id).get();
+
+	// then
+	assertThat(student).usingRecursiveComparison().ignoringFields("id").isEqualTo(createStudent());
+  }
+
+  Student createStudent() {
+	return Student.builder()
+		.age(22)
+		.name("John")
+		.build();
+  }
+}
