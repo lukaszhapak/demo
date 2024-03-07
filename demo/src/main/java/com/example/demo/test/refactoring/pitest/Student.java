43201d9419ca1a04e24b694883b33a313725af03
lukasz.hapak <lukaszrdw@gmail.com>
2024-03-07 11:47:37 +0100

    spock pitest

--- demo/src/main/java/com/example/demo/test/refactoring/pitest/Student.java
+++ demo/src/main/java/com/example/demo/test/refactoring/pitest/Student.java
@@ -1,8 +1,12 @@
 package com.example.demo.test.refactoring.pitest;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
+@Getter
+@Setter
 @ToString
 @AllArgsConstructor
 class Student {
--- demo/src/test/groovy/com/example/demo/test/refactoring/pitest/StudentFacadeSpec.groovy
+++ demo/src/test/groovy/com/example/demo/test/refactoring/pitest/StudentFacadeSpec.groovy
@@ -10,11 +10,9 @@ class StudentFacadeSpec extends Specification {
     StudentRepository studentRepository = Mock()
     MessagePublisher messagePublisher = Mock()
     StudentFacade studentFacade = new StudentConfiguration().studentFacade(studentRepository, messagePublisher)
+    Student student = new Student("John", 24)
 
     def "should save valid student"() {
-        given:
-        Student student = new Student("John", 24)
-
         when:
         Student response = studentFacade.save(student)
 
@@ -22,5 +20,31 @@ class StudentFacadeSpec extends Specification {
         assertThat(response).usingRecursiveComparison().isEqualTo(student)
         1 * messagePublisher.publishStudentSavedEvent(student)
         1 * studentRepository.save(student)
+    }
+
+    def "should throw exception when name is too short"() {
+        given:
+        student.setName("J")
+
+        when:
+        studentFacade.save(student)
+
+        then:
+        thrown IllegalArgumentException
+        0 * messagePublisher.publishStudentSavedEvent(student)
+        0 * studentRepository.save(student)
+    }
+
+    def "should throw exception when age is too high"() {
+        given:
+        student.setAge(131)
+
+        when:
+        studentFacade.save(student)
+
+        then:
+        thrown IllegalArgumentException
+        0 * messagePublisher.publishStudentSavedEvent(student)
+        0 * studentRepository.save(student)
     }
 }
