898bdbf1994289b43e641b88d7938571a851ca4d
lukasz.hapak <lukaszrdw@gmail.com>
2024-03-20 22:41:26 +0100

    properties refactor

--- src/test/java/com/example/demo/commons/AbstractIntegrationTest.java
+++ src/test/java/com/example/demo/commons/AbstractIntegrationTest.java
@@ -1,8 +1,10 @@
 package com.example.demo.commons;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public abstract class AbstractIntegrationTest {
 
 }
