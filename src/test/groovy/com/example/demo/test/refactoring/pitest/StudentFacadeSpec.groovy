f2e936df175b538fa9f8e3151c435b585638f5fc
lukasz.hapak <lukaszrdw@gmail.com>
2024-04-02 20:12:23 +0200

    refactor

--- src/test/groovy/com/example/demo/test/refactoring/pitest/StudentFacadeSpec.groovy
+++ src/test/groovy/com/example/demo/test/refactoring/pitest/StudentFacadeSpec.groovy
@@ -9,41 +9,41 @@ class StudentFacadeSpec extends Specification {
     StudentRepository studentRepository = Mock()
     StudentEventPublisher messagePublisher = Mock()
     StudentFacade studentFacade = new StudentConfiguration().studentFacade(studentRepository, messagePublisher)
-    Student student = new Student("John", 24)
+    Student validStudent = new Student("John", 24)
 
     def "should save valid student"() {
         when:
-        Student response = studentFacade.save(student)
+        Student response = studentFacade.save(validStudent)
 
         then:
-        assertThat(response).usingRecursiveComparison().isEqualTo(student)
-        1 * messagePublisher.publishStudentSavedEvent(student)
-        1 * studentRepository.save(student)
+        assertThat(response).usingRecursiveComparison().isEqualTo(validStudent)
+        1 * messagePublisher.publishStudentSavedEvent(validStudent)
+        1 * studentRepository.save(validStudent)
     }
 
     def "should throw exception when name is too short"() {
         given:
-        student.setName("J")
+        validStudent.setName("J")
 
         when:
-        studentFacade.save(student)
+        studentFacade.save(validStudent)
 
         then:
         thrown IllegalArgumentException
-        0 * messagePublisher.publishStudentSavedEvent(student)
-        0 * studentRepository.save(student)
+        0 * messagePublisher.publishStudentSavedEvent(validStudent)
+        0 * studentRepository.save(validStudent)
     }
 
     def "should throw exception when age is too high"() {
         given:
-        student.setAge(131)
+        validStudent.setAge(131)
 
         when:
-        studentFacade.save(student)
+        studentFacade.save(validStudent)
 
         then:
         thrown IllegalArgumentException
-        0 * messagePublisher.publishStudentSavedEvent(student)
-        0 * studentRepository.save(student)
+        0 * messagePublisher.publishStudentSavedEvent(validStudent)
+        0 * studentRepository.save(validStudent)
     }
 }
