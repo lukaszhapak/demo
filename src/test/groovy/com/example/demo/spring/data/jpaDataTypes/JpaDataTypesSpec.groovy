893522991bcdf47a865d21d30b52f27819e5a1c6
lukasz.hapak <lukaszrdw@gmail.com>
2024-04-04 00:10:14 +0200

    test to spock

---
+++ src/test/groovy/com/example/demo/spring/data/jpaDataTypes/JpaDataTypesSpec.groovy
@@ -0,0 +1,66 @@
+package com.example.demo.spring.data.jpaDataTypes
+
+import com.example.demo.spring.data.specification.SampleData
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.jdbc.core.JdbcOperations
+import spock.lang.Specification
+
+import java.util.stream.Collectors
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat
+
+@SpringBootTest
+class JpaDataTypesSpec extends Specification implements SampleData {
+
+    @Autowired
+    JdbcOperations jdbcOperations
+
+    @Autowired
+    StudentService studentService
+
+    def "should fetch table names"() {
+        expect:
+        fetchTables().size() == 4
+    }
+
+    def "should save and find student"() {
+        given:
+        Long id = studentService.save(createStudent()).getId()
+
+        when:
+        Student student = studentService.findById(id)
+
+        then:
+        assertThat(student).usingRecursiveComparison().ignoringFields("id", "oneToOne.id", "oneToMany.id").isEqualTo(createStudent())
+    }
+
+    List<String> fetchTables() {
+        def tables = jdbcOperations.queryForList("SHOW TABLES").stream().map(Map::values)
+                .map(x -> x.stream().findFirst().get()).collect(Collectors.toList())
+        println(tables)
+        return tables as List<String>
+    }
+
+    Student createStudent() {
+        Student.builder()
+                .firstName("John")
+                .lastName("Doe")
+                .age(24)
+                .gradesArray(new Integer[]{1, 2, 3, 4, 5, 6})
+                .gradesList(List.of(6, 5, 4, 3, 2, 1))
+                .address(Address.builder()
+                        .streetName("Street")
+                        .flatNumber("22")
+                        .build())
+                .oneToOne(StudentOneToOne.builder()
+                        .name("159")
+                        .build())
+                .oneToMany(List.of(
+                        StudentOneToMany.builder().name("123").build(),
+                        StudentOneToMany.builder().name("456").build(),
+                        StudentOneToMany.builder().name("789").build()
+                ))
+                .build()
+    }
+}
