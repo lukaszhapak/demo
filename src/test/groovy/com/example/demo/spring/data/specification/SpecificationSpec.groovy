aeec4f72ac3a573dc8436ea0731e7a3a11fa04c4
lukasz.hapak <lukaszrdw@gmail.com>
2024-04-04 18:54:30 +0200

    refactor

--- src/test/groovy/com/example/demo/spring/data/specification/SpecificationSpec.groovy
+++ src/test/groovy/com/example/demo/spring/data/specification/SpecificationSpec.groovy
@@ -11,7 +11,7 @@ import java.time.LocalDateTime
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat
 
 @SpringBootTest
-class SpecificationSpec extends Specification implements SampleData {
+class SpecificationSpec extends Specification {
 
     @Autowired
     StudentService studentService
@@ -122,7 +122,46 @@ class SpecificationSpec extends Specification implements SampleData {
 
     def saveStudents(Student... students) {
         for (Student student : students) {
-            studentService.save(student);
+            studentService.save(student)
         }
     }
+
+    Student john = Student.builder()
+            .firstName("John")
+            .lastName("Doe")
+            .age(24)
+            .date(LocalDateTime.of(2024, 2, 25, 0, 0, 0))
+            .address(Address.builder()
+                    .streetName("Oak street")
+                    .flatNumber("51")
+                    .build())
+            .build()
+
+    Student jim = Student.builder()
+            .firstName("Jim")
+            .lastName("Newman")
+            .age(21)
+            .date(LocalDateTime.of(2024, 2, 10, 0, 0, 0))
+            .address(Address.builder()
+                    .streetName("School street")
+                    .flatNumber("12")
+                    .build())
+            .build()
+
+    Student michael = Student.builder()
+            .firstName("Michael")
+            .lastName("Smith")
+            .age(27)
+            .date(LocalDateTime.of(2024, 3, 1, 0, 0, 0))
+            .address(Address.builder()
+                    .streetName("Student street")
+                    .flatNumber("123")
+                    .build())
+            .build()
+
+    StudentSearchCriteria studentSearchCriteria = StudentSearchCriteria.builder()
+            .page(0)
+            .size(10)
+            .sortBy("id")
+            .build()
 }
