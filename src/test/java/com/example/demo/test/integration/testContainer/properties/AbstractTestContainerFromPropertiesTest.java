d07ce406629b794ba04139b52c168a53889cf4b3
lukasz.hapak <lukaszrdw@gmail.com>
2024-04-07 22:05:06 +0200

    comment with links

--- src/test/java/com/example/demo/test/integration/testContainer/properties/AbstractTestContainerFromPropertiesTest.java
+++ src/test/java/com/example/demo/test/integration/testContainer/properties/AbstractTestContainerFromPropertiesTest.java
@@ -5,6 +5,8 @@ import com.example.demo.test.integration.SampleProducts;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+// https://github.com/testcontainers/workshop/blob/main/step-4-your-first-testcontainers-integration.md
+// https://java.testcontainers.org/modules/databases/jdbc/
 @SpringBootTest(properties = {
 	"spring.datasource.url=jdbc:tc:postgresql:14.0://demo"
 })
