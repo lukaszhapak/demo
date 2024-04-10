68d307be9866f014c2118c35f6e0c90ddc9a1518
lukasz.hapak <lukaszrdw@gmail.com>
2024-04-10 23:40:58 +0200

    mock mvc spec

---
+++ src/test/groovy/com/example/demo/common/AbstractMockMvcIntegrationSpec.groovy
@@ -0,0 +1,71 @@
+package com.example.demo.common
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.web.servlet.MockMvc
+import spock.lang.Specification
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+abstract class AbstractMockMvcIntegrationSpec extends Specification {
+
+    ObjectMapper objectMapper = new ObjectMapper().registerModule(new JavaTimeModule())
+
+    @Autowired
+    MockMvc mockMvc
+
+    <T> T getHttpCall(String url, int expectedStatusCode, Class<T> returnType) {
+        try {
+            return objectMapper.readValue(mockMvc.perform(get(url))
+                    .andExpect(status().is(expectedStatusCode))
+                    .andReturn().getResponse()
+                    .getContentAsString(), returnType)
+        } catch (Exception e) {
+            throw new RuntimeException(e)
+        }
+    }
+
+    <T> T postHttpCall(String url, int expectedStatusCode, Object body, Class<T> returnType) {
+        try {
+            return objectMapper.readValue(mockMvc.perform(post(url)
+                    .content(objectMapper.writeValueAsString(body))
+                    .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().is(expectedStatusCode))
+                    .andReturn().getResponse()
+                    .getContentAsString(), returnType)
+        } catch (Exception e) {
+            throw new RuntimeException(e)
+        }
+    }
+
+    <T> T putHttpCall(String url, int expectedStatusCode, Object body, Class<T> returnType) {
+        try {
+            return objectMapper.readValue(mockMvc.perform(put(url)
+                    .content(objectMapper.writeValueAsString(body))
+                    .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().is(expectedStatusCode))
+                    .andReturn().getResponse()
+                    .getContentAsString(), returnType)
+        } catch (Exception e) {
+            throw new RuntimeException(e)
+        }
+    }
+
+    void deleteHttpCall(String url, int expectedStatusCode) {
+        try {
+            mockMvc.perform(delete(url))
+                    .andExpect(status().is(expectedStatusCode))
+        } catch (Exception e) {
+            throw new RuntimeException(e)
+        }
+    }
+}
---
+++ src/test/groovy/com/example/demo/spring/http/server/rest/MockMvcSpec.groovy
@@ -0,0 +1,41 @@
+package com.example.demo.spring.http.server.rest
+
+import com.example.demo.common.AbstractMockMvcIntegrationSpec
+
+class MockMvcSpec extends AbstractMockMvcIntegrationSpec {
+
+    def "should get with single param in url"() {
+        expect:
+        getHttpCall("/api/param/single?singleParam=42112", 200, ResponseDTO).getSingleParam() == "42112"
+    }
+
+    def "should get list of students"() {
+        when:
+        List<Student> list = getHttpCall("/api/student", 200, Student[])
+
+        then:
+        list.size() == 2
+    }
+
+    def "should post student"() {
+        when:
+        Student student = postHttpCall("/api/student", 200, new Student(2, "Jim", 42), Student)
+
+        then:
+        student.getId() == 2
+    }
+
+    def "should put student"() {
+        when:
+        Student student = putHttpCall("/api/student/2", 200, new Student(2, "Jim", 42), Student)
+
+        then:
+        student.getId() == 2
+    }
+
+    def "should delete student"() {
+        expect:
+        deleteHttpCall("/api/student/2", 200)
+    }
+}
+
