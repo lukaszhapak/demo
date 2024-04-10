0728c9e7625e03beb786c1594ebca6e5d3914131
lukasz.hapak <lukaszrdw@gmail.com>
2024-04-10 23:17:52 +0200

    rest assured spec

--- src/main/java/com/example/demo/spring/http/server/rest/Student.java
+++ src/main/java/com/example/demo/spring/http/server/rest/Student.java
@@ -1,8 +1,5 @@
 package com.example.demo.spring.http.server.rest;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,15 +9,12 @@ import lombok.ToString;
 
 @Getter
 @Setter
-@Entity
 @Builder
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
 class Student {
 
-  @Id
-  @GeneratedValue
   private Long id;
   private String name;
   private int age;
