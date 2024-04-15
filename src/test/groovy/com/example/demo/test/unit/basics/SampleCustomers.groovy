d4ebac10df64778671bfe3f4c2e2ccf48cb9ac4e
lukasz.hapak <lukaszrdw@gmail.com>
2024-04-15 23:45:03 +0200

    refactor

--- src/test/groovy/com/example/demo/test/unit/basics/SampleCustomers.groovy
+++
@@ -1,9 +0,0 @@
-package com.example.demo.test.unit.basics
-
-import com.example.demo.test.unit.basics.customer.Customer
-
-trait SampleCustomers {
-
-    Customer validCustomer = new Customer("John", 23)
-
-}
--- src/test/groovy/com/example/demo/test/unit/basics/TraitSpockTest.groovy
+++ src/test/groovy/com/example/demo/test/unit/basics/TraitSpockTest.groovy
@@ -1,19 +1,19 @@
 package com.example.demo.test.unit.basics
 
-
+import com.example.demo.test.unit.SampleCustomers
 import spock.lang.Specification
 
 class TraitSpockTest extends Specification implements SampleCustomers {
 
     def "should get customer from trait and change his name to Michael"() {
         expect:
-        validCustomer.getName() == "John"
-        validCustomer.setName("Michael")
+        john.getName() == "John"
+        john.setName("Michael")
     }
 
     def "should get customer from trait and change his name to Jim"() {
         expect:
-        validCustomer.getName() == "John"
-        validCustomer.setName("Jim")
+        john.getName() == "John"
+        john.setName("Jim")
     }
 }
