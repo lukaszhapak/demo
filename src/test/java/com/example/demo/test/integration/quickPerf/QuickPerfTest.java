60f611361d52b55ff62cbb1405c5ce09ec6a8468
lukasz.hapak <lukaszrdw@gmail.com>
2024-05-16 00:03:21 +0200

    quick perf clean up

--- src/test/java/com/example/demo/test/integration/quickPerf/QuickPerfTest.java
+++ src/test/java/com/example/demo/test/integration/quickPerf/QuickPerfTest.java
@@ -4,18 +4,15 @@ import com.example.demo.test.integration.Product;
 import com.example.demo.test.integration.ProductRepository;
 import com.example.demo.test.integration.TestData;
 import org.junit.jupiter.api.Test;
-import org.quickperf.spring.sql.QuickPerfSqlConfig;
 import org.quickperf.sql.annotation.ExpectDelete;
 import org.quickperf.sql.annotation.ExpectInsert;
 import org.quickperf.sql.annotation.ExpectSelect;
 import org.quickperf.sql.annotation.ExpectUpdate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 
 @SpringBootTest
 @org.quickperf.junit5.QuickPerfTest
-@Import({QuickPerfSqlConfig.class})
 class QuickPerfTest {
 
   @Autowired
