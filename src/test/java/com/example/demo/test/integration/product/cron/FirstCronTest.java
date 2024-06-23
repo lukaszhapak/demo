5e5bc9b005111f331cc91fc93b0687a7f1e85870
lukasz.hapak <lukaszrdw@gmail.com>
2024-06-23 19:01:08 +0200

    cron tests

---
+++ src/test/java/com/example/demo/test/integration/product/cron/FirstCronTest.java
@@ -0,0 +1,33 @@
+package com.example.demo.test.integration.product.cron;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import com.example.demo.test.integration.product.data.Product;
+import com.example.demo.test.integration.product.service.ProductService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(properties = {"product.cron=-"})
+class FirstCronTest {
+
+  @Autowired
+  ProductJob productJob;
+
+  @Autowired
+  ProductService productService;
+
+  @Test
+  @DisplayName("should save product and run job")
+  void shouldSaveProductAndRunJob() {
+	// given
+	Long id = productService.save(new Product().setName("asd")).getId();
+
+	// when
+	productJob.setSomeValue();
+
+	// then
+	assertThat(productService.getById(id).isCronValue()).isTrue();
+  }
+}
