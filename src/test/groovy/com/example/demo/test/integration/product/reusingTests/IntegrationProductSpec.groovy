2244317e66edc33bef8fc269739085cfe9a55e94
lukasz.hapak <lukaszrdw@gmail.com>
2024-07-09 16:52:41 +0200

    reusing tests

---
+++ src/test/groovy/com/example/demo/test/integration/product/reusingTests/IntegrationProductSpec.groovy
@@ -0,0 +1,64 @@
+package com.example.demo.test.integration.product.reusingTests
+
+import com.example.demo.test.integration.product.data.Product
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class IntegrationProductSpec extends AbstractProductSpec {
+
+    ObjectMapper objectMapper = new ObjectMapper().registerModule(new JavaTimeModule())
+
+    @Autowired
+    MockMvc mockMvc
+
+    @Override
+    Product saveProduct(Product product) {
+        /// using http or message queue or whatever
+
+        postHttpCall("/api/product", 200, sampleProduct, Product.class)
+    }
+
+    @Override
+    Product getProduct(long id) {
+        getHttpCall("/api/product/" + id, 200, Product.class)
+    }
+
+    <T> T getHttpCall(String url, int expectedStatusCode, Class<T> returnType) {
+        try {
+            return objectMapper.readValue(mockMvc.perform(get(url))
+                    .andDo(print())
+                    .andExpect(status().is(expectedStatusCode))
+                    .andReturn().getResponse()
+                    .getContentAsString(), returnType)
+        } catch (Exception e) {
+            throw new RuntimeException(e)
+        }
+    }
+
+    <T> T postHttpCall(String url, int expectedStatusCode, Object body, Class<T> returnType) {
+        try {
+            return objectMapper.readValue(mockMvc.perform(post(url)
+                    .content(objectMapper.writeValueAsString(body))
+                    .contentType(MediaType.APPLICATION_JSON))
+                    .andDo(print())
+                    .andExpect(status().is(expectedStatusCode))
+                    .andReturn().getResponse()
+                    .getContentAsString(), returnType)
+        } catch (Exception e) {
+            throw new RuntimeException(e)
+        }
+    }
+
+}
---
+++ src/test/groovy/com/example/demo/test/integration/product/reusingTests/UnitProductSpec.groovy
@@ -0,0 +1,26 @@
+package com.example.demo.test.integration.product.reusingTests
+
+
+import com.example.demo.test.integration.product.data.Product
+import com.example.demo.test.integration.product.service.ProductService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UnitProductSpec extends AbstractProductSpec {
+
+    // lets pretend this is unit test
+
+    @Autowired
+    ProductService productService
+
+    @Override
+    Product saveProduct(Product product) {
+        productService.save(product)
+    }
+
+    @Override
+    Product getProduct(long id) {
+        productService.getById(id)
+    }
+}
