29ca52deca50a97ed7183e9583254d52cdeaae16
lukasz.hapak <lukaszrdw@gmail.com>
2024-07-09 15:20:39 +0200

    refactor

--- src/test/groovy/com/example/demo/test/integration/product/io/data/testData/insert/runner/RunnerInsertSpec.groovy
+++ src/test/groovy/com/example/demo/test/integration/product/io/data/testData/insert/runner/RunnerInsertSpec.groovy
@@ -1,5 +1,6 @@
 package com.example.demo.test.integration.product.io.data.testData.insert.runner
 
+import com.example.demo.test.integration.product.SampleProducts
 import com.example.demo.test.integration.product.service.ProductService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -8,13 +9,16 @@ import spock.lang.Specification
 
 @SpringBootTest
 @Import(DataInsertRunnerConfig.class)
-class RunnerInsertSpec extends Specification {
+class RunnerInsertSpec extends Specification implements SampleProducts {
 
     @Autowired
     ProductService productService
 
     def "should get product"() {
         expect:
-        productService.getByName("Phone") != null
+        productService.getByName(sampleProduct.getName()) != null
+
+        // test created new instance of sample product so id is null
+        sampleProduct.getId() == null
     }
 }
