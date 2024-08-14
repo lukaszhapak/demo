9c32d7aef3d87eb886f5cb74974197052228e778
lukasz.hapak <lukaszrdw@gmail.com>
2024-08-14 20:03:45 +0200

    query methods refactor

---
+++ src/main/java/com/example/demo/spring/data/jpa/queryMethods/StudentDTO.java
@@ -0,0 +1,16 @@
+package com.example.demo.spring.data.jpa.queryMethods;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+class StudentDTO {
+
+  private String name;
+  private int age;
+}
